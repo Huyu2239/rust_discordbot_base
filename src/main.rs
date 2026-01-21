8b29bd85aad684f30093220b3a293f85fdec8b0c
Itakiyo Naomasa <main@huyu.dev>
2026-01-21 09:50:25 +0000

    Revert "refactor: boolを直接"
    This reverts commit 08a110a643f0a482bc1eb3664e2be85931221965.

--- src/main.rs
+++ src/main.rs
@@ -39,8 +39,8 @@ async fn main() -> anyhow::Result<()> {
 }
 
 fn is_cargo_run() -> bool {
-    std::env::var("CARGO").is_ok() && std::env::var("CARGO_BIN_NAME").is_ok()
-}
-
-
+    if std::env::var("CARGO").is_ok() && std::env::var("CARGO_BIN_NAME").is_ok() {
+        return true;
+    }
+    false
 }
